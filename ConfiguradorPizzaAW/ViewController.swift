f9e656ffa28910e0b135872a666e52484c7b7d9b
Sandra Ortega <javierhernandolaraospina@macbook-air-de-javier.local>
2016-03-19 21:38:04 -0500

    Initial Commit

---
+++ ConfiguradorPizzaAW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConfiguradorPizzaAW
+//
+//  Created by Javier Hernando Lara Ospina on 19/03/16.
+//  Copyright © 2016 BocachicoSE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
